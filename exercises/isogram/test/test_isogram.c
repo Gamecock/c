bb61fde5e0da07809f9a0811be64ff213504dc96
Mike Finch <mikefinch87@gmail.com>
2017-09-30 23:05:37 -0400

    Updated isogram test

--- exercises/isogram/test/test_isogram.c
+++ exercises/isogram/test/test_isogram.c
@@ -13,49 +13,49 @@ void test_empty_string(void)
 
 void test_lower_case_only(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE(); // delete this line to run test
    TEST_ASSERT_TRUE(isIsogram("isogram"));
 }
 
 void test_duplicated_letter(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_FALSE(isIsogram("eleven"));
 }
 
 void test_longest_known_isogram(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_TRUE(isIsogram("subdermatoglyphic"));
 }
 
 void test_duplicated_letter_mixed_case(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_FALSE(isIsogram("Alphabet"));
 }
 
 void test_non_letter_char(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_TRUE(isIsogram("thumbscrew-japingly"));
 }
 
 void test_duplicated_non_letter_char(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_TRUE(isIsogram("Hjelmqvist-Gryb-Zock-Pfund-Wax"));
 }
 
 void test_multiple_whitespace(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_TRUE(isIsogram("Emily Jung Schwartzkopf"));
 }
 
 void test_duplicated_letter_within_word(void)
 {
-   TEST_IGNORE_MESSAGE("Delete this line after the above test passes");
+   TEST_IGNORE();
    TEST_ASSERT_FALSE(isIsogram("accentor"));
 }
 
